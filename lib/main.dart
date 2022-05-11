122e12a2746c86b47e5f1c790c70e3ff20597a57
Liakot-ali <liakotali2k18@gmail.com>
2022-05-11 23:22:18 +0600

    button added in body

--- lib/main.dart
+++ lib/main.dart
@@ -10,65 +10,74 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.grey[350],
-          title: Row(
+          appBar: AppBar(
+            backgroundColor: Colors.grey[350],
+            title: Row(
+              children: [
+                GestureDetector(
+                  onDoubleTap: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      new SnackBar(
+                        content: Text("Image clicked"),
+                      ),
+                    );
+                  },
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/shimu.jpeg'),
+                  ),
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Liakot Fashion',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                      ),
+                    ),
+                    Text(
+                      "Remain: 0",
+                      style: TextStyle(
+                        color: Colors.green,
+                        fontSize: 15,
+                      ),
+                    ),
+                  ],
+                ),
+                //------------for set align------------
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Container(
+                      child: Icon(
+                        Icons.search,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          body: Column(
             children: [
-              GestureDetector(
-                onDoubleTap: () {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    new SnackBar(
-                      content: Text("Image clicked"),
-                    ),
-                  );
-                },
-                child: CircleAvatar(
-                  backgroundImage: AssetImage('assets/shimu.jpeg'),
-                ),
+              SizedBox(height: 5,),
+              TextButton(
+                onPressed: () {},
+                child: Text("Add"),
               ),
-              SizedBox(
-                width: 5,
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Liakot Fashion',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                    ),
-                  ),
-                  Text(
-                    "Remain: 0",
-                    style: TextStyle(
-                      color: Colors.green,
-                      fontSize: 15,
-                    ),
-                  ),
-                ],
-              ),
-              //------------for set align------------
-              Expanded(
-                child: Align(
-                  alignment: Alignment.centerRight,
-                  child: Container(
-                    child: Icon(
-                      Icons.search,
-                      color: Colors.black,
-                    ),
-                  ),
-                ),
+              SizedBox(height: 5,),
+              Container(
+                alignment: Alignment.center,
+                child: Text("This is body"),
               ),
             ],
-          ),
-        ),
-        body: Container(
-          alignment: Alignment.center,
-          child: Text("This is body"),
-        ),
-      ),
+          )),
     );
   }
 }
