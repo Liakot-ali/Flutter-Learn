48108f90619e2a20dd49cf07b5cff75ecf22b69d
Liakot-ali <liakotali2k18@gmail.com>
2022-05-22 19:18:30 +0600

    connect different class

---
+++ lib/customer_added.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import './customers.dart';
+
+class CustomerAdder extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _CustomerAdderState();
+  }
+}
+
+class _CustomerAdderState extends State<CustomerAdder> {
+  List<String> customers = ['Liakot', 'Jannat', 'Sonu', 'Umar'];
+  List<String> address = ["Bangladesh", "Rangpur", "Nepal", "Nizeria"];
+  var clicked = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(children: [
+      Container(
+        margin: EdgeInsets.all(5),
+        child: TextButton(
+          onPressed: () {
+            setState(() {
+              customers.add("Shimu");
+              clicked++;
+            });
+          },
+          child: Text("Add"),
+        ),
+      ),
+      Customers(customers, clicked)
+    ]);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,106 +1,72 @@
 import 'package:flutter/material.dart';
+import 'customer_added.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  List<String> customers = ['Liakot', 'Jannat', 'Sonu', 'Umar'];
-  List<String> address = ["Bangladesh", "Rangpur", "Nepal", "Nizeria"];
-  var clicked = 0;
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.grey[350],
-            title: Row(
-              children: [
-                GestureDetector(
-                  onDoubleTap: () {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      new SnackBar(
-                        content: Text("Image clicked"),
-                      ),
-                    );
-                  },
-                  child: CircleAvatar(
-                    backgroundImage: AssetImage('assets/shimu.jpeg'),
-                  ),
-                ),
-                SizedBox(
-                  width: 5,
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Liakot Fashion',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                      ),
-                    ),
-                    Text(
-                      "Remain: 0",
-                      style: TextStyle(
-                        color: Colors.green,
-                        fontSize: 15,
-                      ),
-                    ),
-                  ],
-                ),
-                //------------for set align------------
-                Expanded(
-                  child: Align(
-                    alignment: Alignment.centerRight,
-                    child: Container(
-                      child: Icon(
-                        Icons.search,
-                        color: Colors.black,
-                      ),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.grey[350],
+              title: Row(
+                children: [
+                  GestureDetector(
+                    onDoubleTap: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        new SnackBar(
+                          content: Text("Image clicked"),
+                        ),
+                      );
+                    },
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage('assets/shimu.jpeg'),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ),
-          body: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.all(5),
-                child: TextButton(
-                  onPressed: () {
-                    setState(() {
-                      customers.add("Shimu");
-                      clicked++;
-                    });
-                  },
-                  child: Text("Add"),
-                ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Liakot Fashion',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                        ),
+                      ),
+                      Text(
+                        "Remain: 0",
+                        style: TextStyle(
+                          color: Colors.green,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                  //------------for set align------------
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: Container(
+                        child: Icon(
+                          Icons.search,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              Text("You have clicked " + clicked.toString() + " times"),
-              Column(
-                children: customers
-                    .map((elemnt) => ListTile(
-                          title: Text(elemnt),
-                          subtitle: Text("This is subtitle"),
-                        ))
-                    .toList(),
-              )
-            ],
-          )),
-    );
+            ),
+            body: Container(
+              child: CustomerAdder(),
+            )));
   }
 }
