296742e99e318546da88322ec16c1c56a5d07632
Liakot-ali <liakotali2k18@gmail.com>
2022-05-22 22:22:40 +0600

    nothing changes

--- lib/customers.dart
+++ lib/customers.dart
@@ -13,12 +13,15 @@ class Customers extends StatelessWidget {
         Text("You have clicked " + clicked.toString() + " times"),
         Expanded(
           child: ListView.builder(
+            scrollDirection: Axis.vertical,
             itemCount: customers.length,
             itemBuilder: (context, index) {
                return ListTile(
                     title: Text(customers[index]),
                     subtitle: Text("This is subtitle"),
-                    onTap: (){},
+                    onTap: (){
+                      
+                    },
                   );
             },), 
           )
