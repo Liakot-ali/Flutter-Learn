9121d171ca1398673aaabef812f583940f56fc2a
Liakot-ali <liakotali2k18@gmail.com>
2022-06-13 21:22:59 +0600

    shop profile updated

--- lib/shop_profile.dart
+++ lib/shop_profile.dart
@@ -12,18 +12,30 @@ class _ShopProfileState extends State<ShopProfile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.grey[350],
-          title: Container(
-            child: appBar("Liakot Fashion", "Remain: 0", "assets/shimu.jpeg"),
-          ),
+      appBar: AppBar(
+        backgroundColor: Colors.grey[350],
+        title: Container(
+          child: appBar("Liakot Fashion", "Remain: 0", "assets/shimu.jpeg"),
         ),
-        body: Center(
-          child: Text(
-            "This is user profile.\nAdd user details here",
-            style: TextStyle(color: Colors.black),
-            maxLines: 1,
+      ),
+      
+      body: Center(
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 10.0),
+              Image.asset("assets/liakot1.jpeg", height: 100.0, width: 100.0,),
+              Text(
+                "Shop Name",
+                style: TextStyle(color: Colors.grey, fontSize: 10.0),
+              ),
+              Text(
+                "Liakot Fashion",
+                style: TextStyle(color: Colors.black, fontSize: 17.0),
+              ),
+            ],
           ),
-        ));
+      ),
+    );
   }
 }
