c80d4c2b2c4bbe62e61566acf1cc472c5c9a5261
Liakot-ali <liakotali2k18@gmail.com>
2022-06-13 21:30:24 +0600

    update button added in shop profile

--- lib/shop_profile.dart
+++ lib/shop_profile.dart
@@ -18,47 +18,62 @@ class _ShopProfileState extends State<ShopProfile> {
           child: appBar("Liakot Fashion", "Remain: 0", "assets/shimu.jpeg"),
         ),
       ),
-      
       body: Center(
         child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(height: 10.0),
-              Image.asset("assets/liakot1.jpeg", height: 100.0, width: 100.0,),
-              Text(
-                "Shop Name",
-                style: TextStyle(color: Colors.grey, fontSize: 10.0),
-              ),
-              Text(
-                "Liakot Fashion",
-                style: TextStyle(color: Colors.black, fontSize: 17.0),
-              ),
-              Text(
-                "Owner Name",
-                style: TextStyle(color: Colors.grey, fontSize: 10.0),
-              ),
-              Text(
-                "Liakot Ali Liton",
-                style: TextStyle(color: Colors.black, fontSize: 17.0),
-              ),
-              Text(
-                "Category",
-                style: TextStyle(color: Colors.grey, fontSize: 10.0),
-              ),
-              Text(
-                "Ladies Fashion",
-                style: TextStyle(color: Colors.black, fontSize: 17.0),
-              ),
-              Text(
-                "Address",
-                style: TextStyle(color: Colors.grey, fontSize: 10.0),
-              ),
-              Text(
-                "Tajnagor, Parbatipur, Dinajpur",
-                style: TextStyle(color: Colors.black, fontSize: 17.0),
-              ),
-            ],
-          ),
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 10.0),
+            Image.asset(
+              "assets/liakot1.jpeg",
+              height: 100.0,
+              width: 100.0,
+            ),
+            Text(
+              "Shop Name",
+              style: TextStyle(color: Colors.grey, fontSize: 10.0),
+            ),
+            Text(
+              "Liakot Fashion",
+              style: TextStyle(color: Colors.black, fontSize: 17.0),
+            ),
+            Text(
+              "Owner Name",
+              style: TextStyle(color: Colors.grey, fontSize: 10.0),
+            ),
+            Text(
+              "Liakot Ali Liton",
+              style: TextStyle(color: Colors.black, fontSize: 17.0),
+            ),
+            Text(
+              "Category",
+              style: TextStyle(color: Colors.grey, fontSize: 10.0),
+            ),
+            Text(
+              "Ladies Fashion",
+              style: TextStyle(color: Colors.black, fontSize: 17.0),
+            ),
+            Text(
+              "Address",
+              style: TextStyle(color: Colors.grey, fontSize: 10.0),
+            ),
+            Text(
+              "Tajnagor, Parbatipur, Dinajpur",
+              style: TextStyle(color: Colors.black, fontSize: 17.0),
+            ),
+            TextButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  new SnackBar(
+                    behavior: SnackBarBehavior.floating,
+                    margin: EdgeInsets.only(bottom: 10.0),
+                    content: Text("Under Construction"),
+                  ),
+                );
+              },
+              child: Text("Update Profile"),
+            ),
+          ],
+        ),
       ),
     );
   }
