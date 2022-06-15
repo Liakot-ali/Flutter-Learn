38dd54918b695fe86f8ff9fef086fd8b56f2a34b
Liakot-ali <liakotali2k18@gmail.com>
2022-06-15 20:35:04 +0600

    cursor size changes

--- lib/customer_details.dart
+++ lib/customer_details.dart
@@ -25,44 +25,44 @@ class _StateCustomerDetails extends State<CustomerDetails> {
           child: appBar(value.name, value.address, value.picture),
         ),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Text(
-              "Customer Name:",
-              style: TextStyle(fontSize: 10.0),
-            ),
-            TextFormField(
-              initialValue: value.name,
-              maxLength: 10,
-            ),
-            SizedBox(
-              height: 5.0,
-            ),
-            Text(
-              "Customer Address:",
-              style: TextStyle(fontSize: 10.0),
-            ),
-            TextFormField(
-              initialValue: value.address,
-              maxLength: 10,
-            ),
-            SizedBox(
-              height: 5.0,
-            ),
-            Text(
-              "Customer Phone:",
-              style: TextStyle(fontSize: 10.0),
-            ),
-            TextFormField(
-              initialValue: "User Phone Number",
-              maxLength: 10,
-            ),
-            SizedBox(
-              height: 5.0,
-            ),
-          ],
-        ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Customer Name:",
+            style: TextStyle(fontSize: 15.0),
+          ),
+          TextFormField(
+            initialValue: value.name,
+            maxLength: 10,
+            cursorHeight: 17.0,
+          ),
+          SizedBox(
+            height: 5.0,
+          ),
+          Text(
+            "Customer Address:",
+            style: TextStyle(fontSize: 15.0),
+          ),
+          TextFormField(
+            initialValue: value.address,
+            maxLength: 10,
+          ),
+          SizedBox(
+            height: 5.0,
+          ),
+          Text(
+            "Customer Phone:",
+            style: TextStyle(fontSize: 15.0),
+          ),
+          TextFormField(
+            initialValue: "User Phone Number",
+            maxLength: 10,
+          ),
+          SizedBox(
+            height: 5.0,
+          ),
+        ],
       ),
     );
   }
