4357849a56d4eb8c00fe7ac98ece2812ba76ac42
Liakot-ali <liakotali2k18@gmail.com>
2022-06-18 23:34:07 +0600

    form added in add customer

--- lib/add_customer.dart
+++ lib/add_customer.dart
@@ -1,12 +1,37 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class addCustomer  extends StatelessWidget {
+class addCustomer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Add Customer"),),
-      body: Center(child: Text("Add Customer added field"),),
-      
+      appBar: AppBar(
+        title: Text("Add Customer"),
+      ),
+      body: Column(
+        children: [
+          TextFormField(
+            initialValue: "Customer Name",
+            maxLength: 25,
+            cursorHeight: 17.0,
+          ),
+          TextFormField(
+            initialValue: "Customer Type",
+            maxLength: 20,
+            cursorHeight: 17.0,
+          ),
+          TextFormField(
+            initialValue: "Customer Phone",
+            maxLength: 11,
+            cursorHeight: 17.0,
+          ),
+          TextFormField(
+            initialValue: "Customer Address",
+            maxLength: 50,
+            cursorHeight: 17.0,
+          ),
+        ],
+      ),
     );
   }
 }
