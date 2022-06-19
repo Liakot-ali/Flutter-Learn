70236437013452cf08dcb7bdcd13172bac837bc6
Liakot-ali <liakotali2k18@gmail.com>
2022-06-19 20:36:06 +0600

    change decoration of name field

--- lib/add_customer.dart
+++ lib/add_customer.dart
@@ -8,29 +8,37 @@ class addCustomer extends StatelessWidget {
       appBar: AppBar(
         title: Text("Add Customer"),
       ),
-      body: Column(
-        children: [
-          TextFormField(
-            initialValue: "Customer Name",
-            maxLength: 25,
-            cursorHeight: 17.0,
-          ),
-          TextFormField(
-            initialValue: "Customer Type",
-            maxLength: 20,
-            cursorHeight: 17.0,
-          ),
-          TextFormField(
-            initialValue: "Customer Phone",
-            maxLength: 11,
-            cursorHeight: 17.0,
-          ),
-          TextFormField(
-            initialValue: "Customer Address",
-            maxLength: 50,
-            cursorHeight: 17.0,
-          ),
-        ],
+      body: Form(
+        child: Column(
+          children: [
+            TextFormField(
+              maxLength: 25,
+              cursorHeight: 17.0,
+              keyboardType: TextInputType.name,
+              decoration: InputDecoration(
+                helperText: "",
+                labelStyle: TextStyle(fontSize: 15.0),
+                labelText: "Name",
+                hintText: "Customer Name",
+              ),
+            ),
+            TextFormField(
+              initialValue: "Customer Type",
+              maxLength: 20,
+              cursorHeight: 17.0,
+            ),
+            TextFormField(
+              initialValue: "Customer Phone",
+              maxLength: 11,
+              cursorHeight: 17.0,
+            ),
+            TextFormField(
+              initialValue: "Customer Address",
+              maxLength: 50,
+              cursorHeight: 17.0,
+            ),
+          ],
+        ),
       ),
     );
   }
