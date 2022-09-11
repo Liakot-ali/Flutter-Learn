6539faff307a531a1c88f0166c17d289e81c3c8b
Liakot-Ali <=>
2022-09-11 12:37:37 +0600

    Creating updateUserProfile file

---
+++ lib/update_customer_profile.dart
@@ -0,0 +1,11 @@
+import 'package:easy_transaction/app_bar.dart';
+import 'package:flutter/material.dart';
+class updateUserProfile extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      
+    );
+  }
+}
